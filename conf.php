704a29304d616dbf12d28a1250ab4714a9f8b6a0
Gerli <gerli.koiv@khk.ee>
2018-01-19 09:49:53 +0200

    Määratud kaustade konstandid

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gerli
+ * Date: 19/01/2018
+ * Time: 09:44
+ */
+
+//kaustade ja failide konstantsed nimetused
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
