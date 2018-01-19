c0f598e8ca7e5ff111b3c21be4c79cacd9f2d5ed
Gerli <gerli.koiv@khk.ee>
2018-01-19 11:01:37 +0200

    kirjeldatud HTML malli faili loaadimise kontroll

--- model/template.php
+++ model/template.php
@@ -10,14 +10,52 @@ class template
 {
     //klassi muutujad
     var $file = ''; //HTML faili malli nimi
-    var $content = false; //ei saa olle ''   //HTML mallist loetud sisu
+    var $content = false; //ei saa olle ''   //HTML malli failist loetud sisu
     var $vars = array(); //HTML malli elementide nimetused ja väärtused
+
+    //HTML malli faili nime ja õiguste kontroll
+    //sisu kugemine, kui tingimused täidetud
+    function loadFile(){
+        if (!is_dir(VIEWS_DIR)){
+            echo 'Ei ole leitud '.VIEWS_DIR.' kataloogi.<br />';
+            exit;
+        }
+        //kui faili nimi on määratud, siis views/failimini.txt
+        $file = $this->file; //abiaesendus
+        if (file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+        //kui faili nimi on määratud, siis views/failimini.txt
+        $file = VIEWS_DIR.$this->file; //abiaesendus
+        if (file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+        //kui faili nimi on määratud kujul
+        //failinimi
+        $file = VIEWS_DIR.$this->file.'.html'; //abiaesendus
+        if (file_exists($file) and is_file($file) and is_readable($file)) {
+            $this->readFile($file);
+        }
+        //kui faili nimi on määratud, siis views/alamkaust/failimini.txt
+        //kui faili nimi on määratud kujul
+        //alamkaust.failinimi   .->/
+        $file = VIEWS_DIR.str_replace('.', '/', $this->file).'.html'; //abiaesendus
+        if (file_exists($file) and is_file($file) and is_readable($file)) {
+            $this->readFile($file);
+        }
+        //kui lehel on sisu puudu
+        if ($this->content === false){
+            echo 'Ei suutnud lugeda '.$this->file.' sisu.<br />';
+            exit;
+        }
+    }
 
     //HTML malli failist sisu lugemine
     function readFile($file){
-        $fp = fopen($file, 'r');
+        /*$fp = fopen($file, 'r');
         $this->content = fread($fp, filesize($file));
-        flose($fp);
+        flose($fp);  OR */
+        $this->content = file_get_contents($file);
 
     }
 }
