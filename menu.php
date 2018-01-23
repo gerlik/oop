77daf94990942c1737ab1c5366701ecde7a27a49
Gerli <gerli.koiv@khk.ee>
2018-01-23 11:06:51 +0200

    loodud objektid menüü ja selle sisu jaoks

---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gerli
+ * Date: 23/01/2018
+ * Time: 11:03
+ */
+
+//loome menüü peamalli objekti
+$menuTmpl = new template('menu.menu');
+//loome elementide malli objekti
+$menuItemTmpl = new template('menu.menu_item');
+
