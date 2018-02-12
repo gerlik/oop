2a207732a364a8887e3a5cb6368bb72de9763c28
Gerli <gerli.koiv@khk.ee>
2018-02-12 08:51:38 +0200

    töötlus

--- index.php
+++ index.php
@@ -9,24 +9,17 @@
 //loeme sisse seadistuse
 require_once 'conf.php';
 
-//uus testobjekt
-//$testTabel = new template('test');
+// loome peamalli objekti template klassist
 $mainTmpl = new template('main');
-
-//lubame kontrollerite haldust
+// lubame kontrollerite haldust
 require_once 'control.php';
-
-$mainTmpl->set('lang','et');
-$mainTmpl->set('page_title','Lehe pealkiri');
+// määrame reaalväärtused malli elementidele
+$mainTmpl->set('lang', 'et');
+$mainTmpl->set('page_title', 'Lehe pealkiri');
 $mainTmpl->set('user', 'Kasutaja');
-$mainTmpl->set('title','Pealkiri');
-$mainTmpl->set('lang_bar','Keeleriba');
-//menüü loomine
+$mainTmpl->set('title', 'Pealkiri');
+$mainTmpl->set('lang_bar', 'Keeleriba');
+// katsetame menüü loomist
 require_once 'menu.php';
 
 echo $mainTmpl->parse();
-//kontrollime control sisu
-$result = $db->getData('SELECT NOW()');
-echo '<pre>';
-print_r($result);
-echo '</pre>';
