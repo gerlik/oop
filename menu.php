d471e7b03365d967bef9383b83cfcd32ebfd3763
Gerli <gerli.koiv@khk.ee>
2018-02-12 08:50:55 +0200

    menüü andmebaasi andmetega

--- menu.php
+++ menu.php
@@ -6,31 +6,23 @@
  * Time: 11:03
  */
 
-//loome menüü peamalli objekti
-$menuTmpl = new template('menu.menu');
-//loome elementide malli objekti
-$menuItemTmpl = new template('menu.menu_item');
-
-//avaleht
-$menuItemTmpl->set('menu_item_name', 'avaleht');
-$link = $http->getLink();
-$menuItemTmpl->set('link', $link);
-$menuTmpl->add('menu_items', $menuItemTmpl->parse());
-
-//esimene
-$menuItemTmpl->set('menu_item_name','esimene');
-//link
-$link = $http->getLink(array('control' => 'esimene'));
-$menuItemTmpl->set('link', $link);
-$menuTmpl->add('menu_items', $menuItemTmpl->parse());
-
-//teine
-$menuItemTmpl->set('menu_item_name','teine');
-//link
-$link = $http->getLink(array('control' => 'teine'));
-$menuItemTmpl->set('link', $link);
-$menuTmpl->add('menu_items', $menuItemTmpl->parse());
-
-//valmis vaade
-$menu = $menuTmpl->parse();
-$mainTmpl->set('menu', $menu);
+// loome menüü peamalli objekti template klassist
+$menu = new template('menu.menu');
+// loome menüü elemendi malli objekti
+$menuItem = new template('menu.menu_item');
+// koostame päringu menüü ja sisu ülesehitamiseks
+$sql = 'SELECT content_id, content, title '.
+    'FROM content WHERE parent_id='.fixDb(0).
+    ' AND show_in_menu='.fixDb(1);
+// küsime andmed andmebaasist
+$result = $db->getData($sql);
+// ehitame menüü
+if($result != false){
+    foreach ($result as $page){
+        $menuItem->set('menu_item_name', $page['title']);
+        $link = $http->getLink(array('page_id'=>$page['content_id']));
+        $menuItem->set('link', $link);
+        $menu->add('menu_items', $menuItem->parse());
+    }
+}
+$mainTmpl->add('menu', $menu->parse());
